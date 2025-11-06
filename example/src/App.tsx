affc84a00479f0301df652007b336ba4c7d70a98
Mohamed Ashraf <mohamed7elprans@yahoo.com>
2025-11-06 07:33:01 +0200

    feat: introduce package UI

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,18 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-mashura';
-
-const result = multiply(3, 7);
+import Mashura from 'react-native-mashura';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Feature Request Page</Text>
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.contentText}>
+          This is a example page for react-native-mashura.
+        </Text>
+        <Mashura />
+      </View>
     </View>
   );
 }
@@ -14,7 +20,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#ddd',
+    paddingTop: 60,
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  content: {
+    flex: 1,
+    padding: 10,
+  },
+  contentText: {
+    fontSize: 16,
+    marginBottom: 10,
+    textAlign: 'center',
   },
 });
---
+++ src/types/requests.ts
@@ -0,0 +1,9 @@
+export type RequestType = {
+  title: string;
+  description: string;
+  votesIds: string[];
+  // numberOfComments: number; // TODO
+  // status: StatusType; // TODO
+};
+
+export type RequestsType = RequestType[];
